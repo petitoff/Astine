7ca3b79cd861c1f75fdc9be17ed3a7431c500c63
petitoff <petitoff@outlook.com>
2022-01-17 16:33:48 +0100

    Dodanie możliwości remisu

--- main.cpp
+++ main.cpp
@@ -356,6 +356,12 @@ void f1vs1()
         int w = textwidth((char *)"Wygral O !!");
         outtextxy((400 - w) / 2, 500, (char *)"Wygral O !!");
     }
+    else if (who_win == 0)
+    {
+        settextstyle(10, 0, 3);
+        int w = textwidth((char *)"Remis");
+        outtextxy((400 - w) / 2, 500, (char *)"Remis");
+    }
 }
 
 void computer(char *a, int turn)
